06f8e65fc08ea50ce25911c9c883a57e44124624
Sam <60146956+srudqvist@users.noreply.github.com>
2021-08-31 10:37:55 -0500

    Almost finished

--- Anagrams.py
+++ Anagrams.py
@@ -13,52 +13,71 @@ def test():
     testPair(2,'Clint Eastwood','Old West Action',True)
     testPair(3,'arranged','deranged',False)
     testPair(4, "32", 32, False)
+    testPair(5, "BigAndSmall", "bIGaNDsMALL", True)
+
     
 	#YOUR ADDITIONAL TESTS GO HERE
   
 #The testPair function takes a test number, two words, and the expected result (true if they are anagrams and false otherwise).  It prints a notice of whether or not the anagram function gave the correct answer for these two words.
 def testPair(testNumber,word1,word2,expected):
 
-    if anagram(word1,word2)==expected:
+    if anagram(testNumber,word1,word2)==expected:
         result = "passed"
     else:
         result = "failed"
-    # Added for testing purposes. 
+    # Added for testing purposes. Is this okay??
     word1 = str(word1)
     word2 = str(word2)
     print("Test "+str(testNumber)+"  "+result+":"+ word1+" "+word2+" "+str(expected))
 
 
 #The anagram function takes two words and returns true if they are anagrams and false otherwise.  
-
-def anagram(word1,word2):
+def anagram(testNumber,word1,word2):    # Is this okay??
 	#YOUR CODE GOES HERE
-
-    # Check if the given words are of type string.
-    if type(word1) != str:
-        print("The first word is not a string, please change word1 to a string.")
-        return False
-
-    if type(word2) != str:
-        print("The second word is not a string, please change word2 to a string.")
-        return False
-
-    # Make sure the words are the same length.
-    str1Length = len(word1)
-    str2Length = len(word2)
-    
-    if str2Length != str1Length:
-        return False
-
-    # Sort the words and check if they contain the same letters.
-    word1 = sorted(word1)
-    word2 = sorted(word2)
-    
-    for i in range(0, str1Length):
-        if word1[i] != word2[i]:
+    try:
+        # Check if the given words are of type string.
+        if type(word1) != str:
+            print("\nThe first word in test", testNumber, "is not a string, please change word1 to a string.\n")
             return False
-    
-    return True
+
+        if type(word2) != str:
+            print("\nThe second word in test", testNumber, "is not a string, please change word2 to a string.\n")
+            return False
+
+        # Make all the letters in the words lower case.
+        word1 = word1.lower()
+        word2 = word2.lower()
+
+        # Ignore spaces.
+        word1 = word1.replace(" ", "")
+        word2 = word2.replace(" ", "")
+
+        # Get the length of the words.
+        str1Length = len(word1)
+        str2Length = len(word2)
+
+        # Sort the words
+        word1 = sorted(word1)
+        word2 = sorted(word2)
+        
+        # Check if the words are the same length.
+        if str2Length != str1Length:
+            return False
+        
+        # Check if the words contain the same letters.
+        for i in range(0, str1Length):
+            if word1[i] != word2[i]:
+                return False
+        
+        return True
+        
+    except:
+        print("\nSomething went wrong, please check all of the inputs and make sure they are strings.\n")
+        ans = str(input("Run again anyways?(y/n) "))
+        if y in ans:
+            test()
+        else:
+            quit()
 
 
 
