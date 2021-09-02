daa5955f7a508e9c7179ded3ef41d1d7edfe7df6
Sam <60146956+srudqvist@users.noreply.github.com>
2021-09-01 23:01:44 -0500

    Finished Anagram

--- Anagrams.py
+++ Anagrams.py
@@ -6,55 +6,67 @@
 ###
 ### Modification Log:
 ### 2021/08/30: Created the file, copied test functions from Dr. Browning. def anagram() is ready to be tested.
-### 2021/08/31: Improvements in error handling has been made. User now gets a message saying what word is not a string.
-###             
+###
 
-"""
-Should header be done like this?
-"""
 
 #The test function conducts a series of tests by calling testPair.
-
 def test():
     testPair(1,'eat','tea',True)
-    testPair(2,'Clint Eastwood','Old West Action',True)
+    testPair(2,'Clint, Eastwood','Old, West Action',True)
     testPair(3,'arranged','deranged',False)
-    testPair(4, "32", 32, False)
     testPair(5, "BigAndSmall", "bIGaNDsMALL", True)
     testPair(6, "Nag a ram", "anagram", True)
     testPair(7, "Slot Machines", "Cash lost in me", True)
     testPair(8, "football", "laundry basket", False)
 
-
-
-
-    
-	#YOUR ADDITIONAL TESTS GO HERE
   
-#The testPair function takes a test number, two words, and the expected result (true if they are anagrams and false otherwise).  It prints a notice of whether or not the anagram function gave the correct answer for these two words.
-def testPair(testNumber,word1,word2,expected):
-
-    if anagram(testNumber,word1,word2)==expected:
+# The testPair function takes a test number, two words, and the expected result (true if they are anagrams and false otherwise).  
+# It prints a notice of whether or not the anagram function gave the correct answer for these two words.
+def testPair(testNumber, word1,word2,expected):
+    if anagram(word1,word2)==expected:
         result = "passed"
     else:
         result = "failed"
-    # Added for testing purposes. Is this okay??
+    
+    print("Test "+str(testNumber)+"  "+result+":"+ word1+" "+word2+" "+str(expected))
+
+# My own test function, it takes a test number, two words, and the expected result (true if they are anagrams and false otherwise).  
+# It prints a notice of whether or not the anagram function gave the correct answer for these two words.
+def myTest():
+    print("\nMy own tests.")
+    myTestPair(1, "32", 32, False)
+    myTestPair(2, 352, "testing", False)
+    myTestPair(3, 675, 9800, False)
+    myTestPair(4, ["my", "list"], "mylist", False)
+
+
+# My own testPair, adopted and modified from Dr. Brownings testPair. it takes a test number, two words, and the expected result (true if they are anagrams and false otherwise).  
+# It prints a notice of whether or not the anagram function gave the correct answer for these two words.
+def myTestPair(testNumber, word1,word2,expected):
+    if anagram(word1,word2)==expected:
+        result = "passed"
+    else:
+        result = "failed"
     word1 = str(word1)
     word2 = str(word2)
     print("Test "+str(testNumber)+"  "+result+":"+ word1+" "+word2+" "+str(expected))
 
 
-#The anagram function takes two words and returns true if they are anagrams and false otherwise.  
-def anagram(testNumber,word1,word2):    # Is this okay??
+
+# The anagram function takes two words and returns true if they are anagrams and false otherwise. 
+# It also takes the testNumber in order to provide a more specific error message. 
+# Punctuation, question marks etc, are treated like characters.
+def anagram(word1,word2): 
 	#YOUR CODE GOES HERE
     try:
         # Check if the given words are of type string.
         if type(word1) != str:
-            print("\nThe first word in test", testNumber, "is not a string, please change word1 to a string.\n")
+            print("\nThe first word is not a string, please change word1 to a string.")
             return False
 
         if type(word2) != str:
-            print("\nThe second word in test", testNumber, "is not a string, please change word2 to a string.\n")
+            print(type(word2))
+            print("\nThe second word is not a string, please change word2 to a string.")
             return False
 
         # Make all the letters in the words lower case.
@@ -86,12 +98,8 @@ def anagram(testNumber,word1,word2):    # Is this okay??
         
     except:
         print("\nSomething went wrong, please check all of the inputs and make sure they are strings.\n")
-        ans = str(input("Run again anyways?(y/n) "))
-        if y in ans:
-            test()
-        else:
-            quit()
 
 
 
 test()
+myTest()
