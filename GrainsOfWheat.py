10279e917d009e344eb5969d5dedc29888992441
Sam <60146956+srudqvist@users.noreply.github.com>
2021-09-16 21:45:09 -0500

    finished grains of wheat

--- GrainsOfWheat.py
+++ GrainsOfWheat.py
@@ -3,6 +3,7 @@
 ### CSCI 0262 Algorithms
 ### 
 ### This program finds the total grains on a board of size n*n 
+### And the time it takes to count all of the grains.
 ### 
 ### Modification Log:
 ### 
@@ -49,7 +50,7 @@ def timeToCount(myfun,dimension):
   days = days[0]
   hours = hours[0]
   minutes = minutes[0]
-
+  #print("Time:", "Seconds:", seconds, "Minutes:", minutes, "Hours:", hours, "Days:", days, "Years:", years)
   return [seconds,minutes,hours,days,years]
 
 
@@ -58,14 +59,26 @@ def testCount():
     print ("Test 1 passed. Number of grains with identity function")
   else:
     print ("Test 1 failed. Number of grains with identity function")
+
   if numberOfGrains(lambda k:k*k,2)==30: 
     print ("Test 2 passed. Number of grains with square function")
   else:
     print ("Test 2 failed. Number of grains with square function")
-  #if numberOfGrains(lambda k:k*k,20)==21413400: 
-   # print ("Test 3 passed. Number of grains with square function")
-  #else:
-   # print ("Test 3 failed. Number of grains with square function")
+
+  if numberOfGrains(lambda k:k**k,2)==288: 
+    print ("Test 5 passed. Number of grains with exponential function")
+  else:
+    print ("Test 5 failed. Number of grains with exponential function")
+
+  if numberOfGrains(lambda k:k-k-1,8)== - 64: 
+    print ("Test 6 passed. Number of grains with linear function, removing one")
+  else:
+    print ("Test 6 failed. Number of grains with linear function, removing one")
+
+  if numberOfGrains(lambda k:2*k - 1,8)== 4096: 
+    print ("Test 7 passed. Count answer part b")
+  else:
+    print ("Test 7 failed. Count answer part b")
 
 
 def testTime():
@@ -73,16 +86,57 @@ def testTime():
     print ("Test 3 passed.  Time to count with identity function.")
   else:
     print ("Test 3 failed.  Time to count with identity function.")
+
   if timeToCount(lambda k:k*k,10)==[10,59,21,3,0]:
     print ("Test 4 passed.  Time to count with square function.")
   else:
     print ("Test 4 failed.  Time to count with square function.")
 
+  if timeToCount(lambda k:k * 2, 8)==[20,9,1,0,0]:
+    print ("Test 8 passed.  Time to count with linear function. Multiply by 2")
+  else:
+    print ("Test 8 failed.  Time to count with linear function. Multiply by 2")
+
+  if timeToCount(lambda k:k + 2, 8)==[48,36,0,0,0]:
+    print ("Test 9 passed.  Time to count with linear function. Add 2.")
+  else:
+    print ("Test 9 failed.  Time to count with linear function. Add 2.")
+  
+  if timeToCount(lambda k:k**k, 3)==[37,48,7,308,12]:
+    print ("Test 9 passed.  Time to count with exponential function.")
+  else:
+    print ("Test 9 failed.  Time to count with exponential function.")
+
+  if timeToCount(lambda k:2**(k-1) ,8)== [15,0,7,26,584942417355]: 
+    print ("Test 11 passed. answer part a")
+  else:
+    print ("Test 11 failed. answer part a")
+
+  if timeToCount(lambda k:2*k - 1,8)== [16,8,1,0,0]: 
+    print ("Test 12 passed. Count answer part b")
+  else:
+    print ("Test 12 failed. Count answer part b")
+
+def underOneDay():
+  for x in range(0, 25):
+    time = timeToCount(lambda k:k,x)
+    if time[3] < 1 and time[4] < 1:
+      print("Dimension:",x)
+      print("Time:",time)
+    else:
+      print("OVERTIME")
+      print("Dimension:",x)
+      print("Overtime time:", time)
+      
+    
 
 print('\ntestCount:\n')
 testCount()
 print('\ntestTime:\n')
 testTime()
+print('\nunderOneDay:\n')
+underOneDay()
+
 
 
 # # Count the time it takes to count all of the grains
